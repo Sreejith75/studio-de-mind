105f397626d66eebaa7287e2b32a5b43dc16146e
Sreejith K <162670518+Sreejith75@users.noreply.github.com>
2025-09-27 20:49:04 +0530

    feat:build files added

--- .next/static/chunks/af84a15d2e740030.js
+++ .next/static/chunks/af84a15d2e740030.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/a32da041b16560ea.js",
   "static/chunks/bfd1182df7bdfb1e.js",
   "static/chunks/779db8c04302b3cd.js",
-  "static/chunks/dadf8c6914129258.css",
-  "static/chunks/turbopack-9467dd73ccf8e8d7.js"
+  "static/chunks/8e425e0c5c663131.css",
+  "static/chunks/turbopack-aaa5f16cf9e7480a.js"
 ])
